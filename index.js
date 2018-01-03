e96e087798b2ab759dbfb1354b46da3b8976e49d
Peter Klinga <pklinga0417@gmail.com>
2018-01-03 14:54:53 +0100

    save

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function colorClock() {
   var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth();
+  var year = date.getFullYear();
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
+  var month = month+1;
   if (seconds < 10) {
     seconds = '0' + seconds;
   }
@@ -12,8 +16,16 @@ function colorClock() {
   if (hours < 10) {
     hours = '0' + hours;
   }
+  if (month < 10) {
+    month = '0' + month;
+  }
+  if (day < 10) {
+    day = '0' + day;
+  }
   var clockFace = hours + ':' + minutes + ':' + seconds;
-
+  var dateFace = year + '.' + month + '.' + day + '.';
+  
+  document.getElementById('day').innerHTML = dateFace;
   document.getElementById('clock').innerHTML = clockFace;
 
   setTimeout(function () {
